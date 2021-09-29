3e1592ef86dc43d4da295147f2d9dbc18cafd870
byjusmx2 <87038490+byjusmx2@users.noreply.github.com>
2021-09-29 17:40:59 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,11 +7,11 @@ groundImage = loadImage("ground2.png")
 }
 function setup() {
 createCanvas(600, 200);
-//create a trex sprite
+//crear sprite de trex
 trex = createSprite(50,160,20,50);
 trex.addAnimation("running", trex_running);
 trex.scale = 0.5;
-//create a ground sprite
+//crear sprite de suelo
 ground = createSprite(200,180,400,20);
 ground.addImage("ground",groundImage);
 ground.x = ground.width /2;
@@ -19,7 +19,7 @@ ground.velocityX = -4;
 }
 function draw() {
 background(220);
-//jump when the space button is pressed
+//hacer que el trex salte al presionar la barra espaciadora
 if (keyDown("space")) {
 trex.velocityY = -10;
 }
